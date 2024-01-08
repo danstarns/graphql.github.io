141269a089a29784a31a1f3f28ce945b1f4646ae
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-01-08 13:50:26 +0100

    add banner

--- website/theme.config.tsx
+++ website/theme.config.tsx
@@ -1,6 +1,18 @@
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs"
+import NextLink from "next/link"
 
 export default {
-  logo: 'GraphQL',
-  docsRepositoryBase: 'https://github.com/graphql/graphql.github.io'
+  banner: {
+    content: (
+      <>
+        📣 GraphQL Conf 2023 • Sept 19-21 • San Francisco •{" "}
+        <NextLink href="/conf" className="underline">
+          Watch the videos
+        </NextLink>
+        !
+      </>
+    ),
+  },
+  logo: "GraphQL",
+  docsRepositoryBase: "https://github.com/graphql/graphql.github.io",
 } satisfies DocsThemeConfig
