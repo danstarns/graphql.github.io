9228e5090e802cf1a96780bdaecff7ed87a1cff4
Dimitri POSTOLOV <dmytropostolov@gmail.com>
2024-01-08 13:24:34 +0100

    more work

---
+++ website/src/pages/_meta.ts
@@ -0,0 +1,16 @@
+export default {
+  index: {
+    type: 'page',
+    display: 'hidden',
+  },
+  learn: {
+    type: 'page',
+    title: 'Learn'
+  },
+  // code: {}
+  spec: {
+    type: 'page',
+    title: 'Spec ↗',
+    href: 'https://spec.graphql.org'
+  }
+}
---
+++ website/theme.config.tsx
@@ -0,0 +1,5 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+export default {
+  logo: 'GraphQL'
+} satisfies DocsThemeConfig
